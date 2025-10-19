9a793b6ecde32740689014e741c70cc717346035
mehulwebdev96@gmail.com <mehulchaudhary330@gmail.com>
2025-10-19 12:59:58 +0530

    updated cookie options

--- src/config/cookieOptions.js
+++ src/config/cookieOptions.js
@@ -1,6 +1,8 @@
+// config/cookieOptions.js
 export const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  maxAge: 60 * 60 * 1000, // 1 hour
+  secure: process.env.NODE_ENV === "production", // true for HTTPS (Render)
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // "none" for cross-site cookies
+  maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+  path: "/", // 👈 ensures cookie is valid for all routes
 };
